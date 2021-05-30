a28ee45e7550adc0d740c67179cb3dec139ff490
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2021-05-30 11:42:29 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EduMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
